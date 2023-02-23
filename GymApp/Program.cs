abc6c350bae72ed1c8c341af03582b2386d6decc
Szemin <116577945+Szemin@users.noreply.github.com>
2023-02-23 21:42:26 +0100

    idk

--- GymApp/Program.cs
+++ GymApp/Program.cs
@@ -4,13 +4,15 @@ class Program
     static void Main()
     {
         Wellcome.WellcomeCardDesign();
-        ClientInfo client = new ClientInfo();
-        client.SayHi();
-        Wellcome.GymProfileCard();
-       
-        UserData.UserProfile();
 
+
+        ClientInfo client = new ClientInfo(); //konstruktor
         
+        UserData userData = new(); //obiekt
+
+        UserData.UserProfile();
+        client.SayHi();
+
 
         Console.ReadKey();
     }
--- GymApp/UserData.cs
+++ GymApp/UserData.cs
@@ -8,15 +8,25 @@ namespace GymApp
 {
     public class UserData
     {
-      
-        
+
+
         public static void UserProfile()
         {
             Console.WriteLine($"First Name: {ClientInfo.GetFirstName()}");
             Console.WriteLine($"Last Name: {ClientInfo.GetLastName()}");
-            Console.WriteLine($"Last Name: {ClientInfo.GetLastName()}");
-            Console.WriteLine($"Last Name: {ClientInfo.GetLastName()}");
-
+            Console.WriteLine($"Last Name: ");
         }
+            public static void GymProfileCard()
+            {
+                Console.WriteLine("                            ");
+                Console.WriteLine("^    -----Gym Member-----    ^");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"First Name:  {ClientInfo.GetFirstName()}");
+                Console.WriteLine($"Last Name:   {ClientInfo.GetLastName()}");
+                Console.WriteLine($"Date of birth:                        ");
+                Console.WriteLine();
+            }
+
+        
     }
 }
